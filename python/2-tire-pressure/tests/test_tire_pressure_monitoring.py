3480fbb8df6a504ea8bd4649076e814cc8721283
Ferdinand Ade <ferdinand.ade@codecentric.de>
2026-03-30 15:56:26 +0200

    add 2 and 3 for python

---
+++ python/2-tire-pressure/tests/test_tire_pressure_monitoring.py
@@ -0,0 +1,6 @@
+from tire_pressure.alarm import Alarm
+
+
+def test_alarm_is_off_by_default():
+    alarm = Alarm()
+    assert not alarm.is_alarm_on
---
+++ python/3-birthday-greetings/tests/test_thing.py
@@ -0,0 +1,5 @@
+from thing.thing import do_thing
+
+
+def test_does_the_thing():
+    assert do_thing() is None
